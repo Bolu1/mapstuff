f7cf0bb321968b1dfdfbb479be0757fe9076482b
Bolu Adetifa <adetifaboluwatife@gmail.com>
2023-06-14 16:58:57 +0100

    push

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
-import React, { Fragment, LegacyRef, useCallback, useRef, useState, createRef, ReactElement } from "react";
+import React, {
+  Fragment,
+  LegacyRef,
+  useCallback,
+  useRef,
+  useState,
+  createRef,
+  ReactElement,
+} from "react";
 import { GoogleMap, Polyline, useLoadScript } from "@react-google-maps/api";
 import FunctionModal from "./Pages/FunctionModal";
 import Screenshots, { Bounds } from "react-screenshots";
 import url from "./image.jpg";
+import * as htmlToImage from "html-to-image";
+
+const createFileName = (extension = "", ...names: any[]) => {
+  if (!extension) {
+    return "";
+  }
+
+  return `${names.join("")}.${extension}`;
+};
 
 interface Bounds {
   x: number;
@@ -13,13 +30,40 @@ interface Bounds {
 }
 
 const App = (): ReactElement => {
-  const polylineRef = useRef<google.maps.Polyline| null>(null);
+  const polylineRef = useRef<google.maps.Polyline | null>(null);
   const listenersRef = useRef<google.maps.MapsEventListener[]>([]);
   const [centerLat, setCenterLat] = useState(51.999889);
   const [centerLng, setCenterLng] = useState(-0.98807);
   const [path, setPath] = useState<google.maps.LatLngLiteral[]>([]);
   const [mapref, setMapRef] = React.useState<google.maps.Map>();
-  const [mapType, setMapType] = useState('satellite')
+  const [mapType, setMapType] = useState("satellite");
+  const [isCompleted, setIsCompleted] = useState(false);
+  const [showFuncion, setShowFunction] = useState(true);
+
+  const ref = createRef<HTMLDivElement>();
+
+  const takeScreenShot = async (node: HTMLElement) => {
+    const dataURI = await htmlToImage.toJpeg(node);
+    return dataURI;
+  };
+
+  const download = (
+    image: string,
+    { name = "img", extension = "jpg" } = {}
+  ) => {
+    const a = document.createElement("a");
+    a.href = image;
+    // base 64 here
+    console.log(a.href);
+    a.download = createFileName(extension, name);
+    a.click();
+  };
+
+  const downloadScreenshot = () =>{
+    setShowFunction(false)
+    takeScreenShot(ref.current as HTMLElement).then(download);
+    setShowFunction(true)
+  }
 
   // Call setPath with new edited path
   const onEdit = useCallback(() => {
@@ -35,14 +79,14 @@ const App = (): ReactElement => {
   // Bind refs to current Polyline and listeners
   const onLoad = useCallback(
     (polyline: google.maps.Polyline) => {
-        polylineRef.current = polyline;
-        const path = polyline.getPath();
-        listenersRef.current.push(
-          path.addListener("set_at", onEdit),
-          path.addListener("insert_at", onEdit),
-          path.addListener("remove_at", onEdit)
-          );
-          console.log(polyline)
+      polylineRef.current = polyline;
+      const path = polyline.getPath();
+      listenersRef.current.push(
+        path.addListener("set_at", onEdit),
+        path.addListener("insert_at", onEdit),
+        path.addListener("remove_at", onEdit)
+      );
+      console.log(polyline);
     },
     [onEdit]
   );
@@ -72,9 +116,9 @@ const App = (): ReactElement => {
     try {
       if (mapref) {
         const newCenter = mapref.getCenter();
-        if(newCenter?.lat() && newCenter?.lng()){
-          setCenterLat( newCenter.lat())
-          setCenterLng( newCenter.lng())
+        if (newCenter?.lat() && newCenter?.lng()) {
+          setCenterLat(newCenter.lat());
+          setCenterLng(newCenter.lng());
           setPath([
             ...path,
             { lat: newCenter.lat(), lng: newCenter.lng() },
@@ -131,35 +175,42 @@ const App = (): ReactElement => {
   if (!isLoaded) return "Loading Maps....";
 
   return (
-    <Fragment>
-      <GoogleMap
-        mapContainerStyle={mapContainerStyle}
-        zoom={10}
-        onLoad={handleOnLoad}
-        onCenterChanged={handleCenterChanged}
-        options={options}
-        center={centre}
-      >
-        <FunctionModal
-          setMapType={setMapType}
-          findCenter={findCenter}
-        />
+    <div ref={ref}>
+      <Fragment>
+        <GoogleMap
+          mapContainerStyle={mapContainerStyle}
+          zoom={10}
+          onLoad={handleOnLoad}
+          onCenterChanged={handleCenterChanged}
+          options={options}
+          center={centre}
+        >
+          {showFuncion && (
+            <FunctionModal
+              downloadScreenshot={downloadScreenshot}
+              isCompleted={isCompleted}
+              setIsCompleted={setIsCompleted}
+              setMapType={setMapType}
+              findCenter={findCenter}
+            />
+          )}
 
-      {
-        path.length > 0 &&
-        <Polyline
-        ref={polylineRef as unknown as LegacyRef<Polyline>}
-        path={path}
-        options={{ editable: true, strokeColor: "#be03fc" }}
-        // Event used when manipulating and adding points
-        onMouseUp={onEdit}
-        onLoad={onLoad}
-        onUnmount={onUnmount}
-        />
-      }
-      </GoogleMap>
-      <button onClick={showPath}>Show Path in Console</button>
-    </Fragment>
+          {path.length > 0 && (
+            <Polyline
+              ref={polylineRef as unknown as LegacyRef<Polyline>}
+              path={path}
+              options={{ editable: true, strokeColor: "#be03fc" }}
+              // Event used when manipulating and adding points
+              onMouseUp={onEdit}
+              onLoad={onLoad}
+              onUnmount={onUnmount}
+            />
+          )}
+        </GoogleMap>
+        <button onClick={downloadScreenshot}>Download screenshot</button>
+        <button onClick={showPath}>Show Path in Console</button>
+      </Fragment>
+    </div>
   );
 };
 
--- src/Pages/FunctionModal.tsx
+++ src/Pages/FunctionModal.tsx
@@ -1,41 +1,78 @@
 import React, { useMemo } from "react";
 
-interface IProps{
-  findCenter: ()=> void
-  setMapType: React.Dispatch<React.SetStateAction<string>>
+interface IProps {
+  downloadScreenshot: ()=> void;
+  findCenter: () => void;
+  isCompleted: boolean;
+  setMapType: React.Dispatch<React.SetStateAction<string>>;
+  setIsCompleted: React.Dispatch<React.SetStateAction<boolean>>;
+  // cancelPolygonHandler: () => void;
 }
 
-const FunctionModal = (props:IProps) => {
+const FunctionModal = (props: IProps) => {
   return (
     <div>
-      <div className="absmodal m-20 bg-white min-w-[20rem] min-h-[20rem] absolute">
-        <button
-          onClick={() => {
-            props.findCenter()
-          }}
-          className="bg-black rounded text-lg p-3 m-6"
-        >
-          Draw
-        </button>
+      <div className="">
+        <div className="absmodal m-20 bg-white min-w-[20rem] min-h-[20rem] absolute">
+          <button
+            onClick={() => {
+              props.findCenter();
+            }}
+            className="bg-black rounded text-lg p-3 m-6"
+          >
+            Draw
+          </button>
 
-        <button
-          onClick={() => {
-            props.setMapType('satellite')
-          }}
-          className="bg-green-800 rounded text-lg p-3 m-6"
-        >
-          Satellite
-        </button>
+          <button
+            onClick={() => {
+              props.setMapType("satellite");
+            }}
+            className="bg-green-800 rounded text-lg p-3 m-6"
+          >
+            Satellite
+          </button>
 
-        <button
-          onClick={() => {
-            props.setMapType('roadmap')
-          }}
-          className="bg-blue-300 rounded text-lg p-3 m-6"
-        >
-          Roadmap
-        </button>
-        
+          <button
+            onClick={() => {
+              props.setMapType("roadmap");
+            }}
+            className="bg-blue-300 rounded text-lg p-3 m-6"
+          >
+            Roadmap
+          </button>
+          <div className=" flex">
+            {props.isCompleted == false ? (
+              <button
+                onClick={() => {
+                  props.setIsCompleted(true);
+                }}
+                className="bg-blue-300 rounded text-lg p-3 m-6"
+              >
+                Done
+              </button>
+            ) : (
+              <button
+                onClick={() => {
+                  props.downloadScreenshot();
+                }}
+                className="bg-blue-300 rounded text-lg p-3 m-6"
+              >
+                Screenshot
+              </button>
+            )}
+
+            <div className="">
+                <button
+                  onClick={() => {
+                    // props.cancelPolygonHandler();
+                  }}
+                  className="bg-red-600 rounded text-lg p-3 m-6"
+                >
+                  Cancel
+                </button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
